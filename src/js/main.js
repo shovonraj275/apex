927155a5b1fed3f07daffb5392da3838ad3c169d
Ezaz ahamed <73046376+shovonraj275@users.noreply.github.com>
2024-03-22 23:50:12 +0600

    js add

---
+++ src/js/main.js
@@ -0,0 +1,16 @@
+let val= 0;
+function cart(){
+    val= val + 1;
+    document.querySelector('.ic_po_ab_cart').innerHTML = val;
+}
+let btn = document.querySelector(".btn_crat");
+btn.addEventListener("click" , cart);
+
+// fav
+let val2= 0;
+function fav(){
+    val2= val2 + 1;
+    document.querySelector('.ic_po_ab_fav').innerHTML = val2;
+}
+let btn3 = document.querySelector(".btn_fav");
+btn3.addEventListener("click" , fav);
