9786e6dd920096f763bc94a9c4bb28282b47804b
Ezaz ahamed <73046376+shovonraj275@users.noreply.github.com>
2024-03-24 10:56:32 +0600

    add cart button

--- src/js/main.js
+++ src/js/main.js
@@ -1,36 +1,42 @@
 
 
-// let cratval = 0;
-// function addcart(){
-//     cratval =cratval+1;
-//     document.querySelector('.ic_po_ab_cart').innerHTML =cratval;
+let cratval = 0;
+function addcart(){
+    cratval =cratval+1;
+    document.querySelector('.ic_po_ab_cart').innerHTML =cratval;
+}
+
+let favVal = 0;
+function addfav(){
+    favVal =favVal+1;
+    document.querySelector('.ic_po_ab_fav').innerHTML =favVal;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let val= 0;
+// function cart(){
+//     val= val + 1;
+//     document.querySelectorAll('.ic_po_ab_cart').innerHTML = val;
 // }
+// let btn = document.querySelectorAll(".btn_crat");
+// btn.addEventListener("click" , cart);
 
-
-
-
-
-
-
-
-
-
-
-
-
-let val= 0;
-function cart(){
-    val= val + 1;
-    document.querySelector('.ic_po_ab_cart').innerHTML = val;
-}
-let btn = document.querySelector(".btn_crat");
-btn.addEventListener("click" , cart);
-
-// fav
-let val2= 0;
-function fav(){
-    val2= val2 + 1;
-    document.querySelector('.ic_po_ab_fav').innerHTML = val2;
-}
-let btn3 = document.querySelector(".btn_fav");
-btn3.addEventListener("click" , fav);
+// // fav
+// let val2= 0;
+// function fav(){
+//     val2= val2 + 1;
+//     document.querySelector('.ic_po_ab_fav').innerHTML = val2;
+// }
+// let btn3 = document.querySelector(".btn_fav");
+// btn3.addEventListener("click" , fav);
